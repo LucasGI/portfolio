20a328ec936481ce227d02c0d6a17d8652add560
LucasGI <jgolsen1203@gmail.com>
2018-04-29 10:32:24 -0600

    update

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     timer();
-    var interval = setInterval(function () { timer(); }, 30000);
-    function timer () {
+    var interval = setInterval(function () { timer(); }, 20000);
+    function timer() {
         var quote = $('#quote').text();
         var quoteGenius = $('#name').text();
         var sourceLength = quoteSource.length;
-        var randomNumber= Math.floor(Math.random()*sourceLength);
-        for(i=0;i<=sourceLength;i+=1){
-        var newQuoteText = quoteSource[randomNumber].quote;
-        var newQuoteGenius = quoteSource[randomNumber].name;
-  var timeAnimationIn = 500;
-  var timeAnimationOut = 5000;
-  var quoteContainer = $('#quote');
-  quoteContainer.fadeOut(timeAnimationIn, function(){
-    quoteContainer.html('');
-            quoteContainer.append('<p>'+newQuoteText+'</p>'+'<p id="name">'+'-'+newQuoteGenius+'</p>');
-    quoteContainer.fadeIn(timeAnimationOut);
-  });   
-        break;
-    };
+        var randomNumber = Math.floor(Math.random() * sourceLength);
+        for (i = 0; i <= sourceLength; i += 1) {
+            var newQuoteText = quoteSource[randomNumber].quote;
+            var newQuoteGenius = quoteSource[randomNumber].name;
+            var timeAnimationIn = 500;
+            var timeAnimationOut = 5000;
+            var quoteContainer = $('#quote');
+            quoteContainer.fadeOut(timeAnimationIn, function () {
+                quoteContainer.html('');
+                quoteContainer.append('<p class="quote">' + newQuoteText + '</p>' + '<p id="name" class="name">' + '-' + newQuoteGenius + '</p>');
+                quoteContainer.fadeIn(timeAnimationOut);
+            });
+            break;
+        };
     }
-    
+
     // var clock = new FlipClock($('.clock'), {
     //     clockFace: 'MinuteCounter',
     //     countDown: true,
--- js/quotes.js
+++ js/quotes.js
@@ -28,10 +28,6 @@ var quoteSource=[
         name:"Ayn Rand"
     },
     {
-        quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-        name:"Ayn Rand"
-    },
-    {
         quote:"Start where you are. Use what you have. Do what you can.",
         name:"Arthur Ashe"
     },
@@ -80,20 +76,151 @@ var quoteSource=[
         name:"George Carlin"
     },
     {
-        quote:"Always do whatever's next.",
-        name:"George Carlin"
-    },
-    {
-        quote:"Atheism is a non-prophet organization.",
-        name:"George Carlin"
-    },
-    {
         quote:"Hapiness is not something ready made. It comes from your own actions.",
         name:"Dalai Lama"
     },
     {
         quote:"Don't confuse education with schooling, I didn't go to havard but people who work for me did.",
         name:"Elon Musk"
-    }
-    
+    },
+    {
+        quote:"Think big and don't listen to people who tell you it can't be done. Life's too short to think small.",
+        name:""
+    },
+    {
+        quote:"Do one thing every day that scares you.",
+        name:""
+    },
+    {
+        quote:"The distance between number one and number two is always a constant. If you want to improve an organization, you have to improve yourself and the organization gets pulled up with you. That is a big lesson.",
+        name:"Indra Nooyi Chairperson and CEO, PepsiCo"
+    },
+    {
+        quote:"Lots of companies don't succeed over time. What do they fundamentally do wrong? They usually miss the future.",
+        name:"Larry Page CEO, Google Inc."
+    },
+    {
+        quote:"There's an entrepreneuer right now, scared to death, making excuses, saying \"It's not the right time just yet.\" There's no such thing as a good time.",
+        name:"Kevin Plank CEO, Under Armour"
+    },
+    {
+        quote:"You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.",
+        name:"Steve Jobs CEO, Apple Inc."
+    },
+    {
+        quote:"Don't worry about failure; you only have to be right once.",
+        name:"Drew Houston Cp-Founder and CEO, Dropbox"
+    },
+    {
+        quote:"If opportunity doesn't knock, build a door.",
+        name:"Milton Berle"
+    },
+    {
+        quote:"A goal should scare you a little, and excite you a lot.",
+        name:"Joe Vitale"
+    },
+    {
+        quote:"A confort zone is a beautiful place, but nothing ever grows there.",
+        name:""
+    },
+    {
+        quote:"Happiness is found when you stop comparing yourself to other people.",
+        name:""
+    },
+    {
+        quote:"Sometimes the smallest step in the right direction ends up being the biggest step of your life. Tip toe if you must, but take the step.",
+        name:""
+    },
+    {
+        quote:"There are no limits to what you can accomplish, except the limits you place on your own thinking.",
+        name:"Brian Tracy"
+    },
+    {
+        quote:"One small crack does not mean that you are broken. It means that you were put to the test and you didn't fall appart.",
+        name:"Linda Poindexter"
+    },
+    {
+        quote:"The one who falls and gets up is so much stronger than the one who never fell.",
+        name:""
+    },
+    {
+        quote:"Make your life a masterpeice; imangine no limitation on what you can be, have or do.",
+        name:"Brian Tracy"
+    },
+    {
+        quote:"F.E.A.R. has two meanings - Forget Everything And Run OR Face Everything And Rise. The choice is yours.",
+        name:""
+    },
+    {
+        quote:"Don't worry about failures, worry about the chances you miss when you don't even try.",
+        name:"Jack Canfield"
+    },
+    {
+        quote:"Be the change that you wish to see in the world.",
+        name:"Mahatma Gandhi"
+    },
+    {
+        quote:"Work hard in silence, let your success be your noise.",
+        name:"Frank Ocean"
+    },
+    {
+        quote:"Those who are crazy enough to think they can change the world usually do.",
+        name:"Steve Jobs"
+    },
+    {
+        quote:"It is better to lead from behind and to put others in front, especially when you celebrate victory when nice things occur. You take the front line when there is danger. Then people will appreciate your leadership.",
+        name:"Nelson Mandela"
+    },
+    {
+        quote:"People ask the difference between a leader and a boss.The leader works in the open, and the boss in covert. The leader leads, and the boss drives.",
+        name:"Theodore Roosevelt"
+    },
+    {
+        quote:"The common question that gets asked in business is, ‘why?’ That’s a good question, but an equally valid question is, ‘why not?’",
+        name:"Jeff Bezos, CEO of Amazon.com"
+    },
+    {
+        quote:"It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you’ll do things differently.",
+        name:"Warren Buffett, CEO of Berkshire Hathaway"
+    },
+    {
+        quote:"A man must be big enough to admit his mistakes, smart enough to profit from them, and strong enough to correct them.",
+        name:"John C. Maxwell"
+    },
+    {
+        quote:"Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart.",
+        name:"Steve Jobs"
+    },
+    {
+        quote:"If you’re changing the world, you’re working on important things. You’re excited to get up in the morning.",
+        name:"Larry Page, CEO of Google"
+    },
+    {
+        quote:"To me, business isn’t about wearing suits or pleasing stockholders. It’s about being true to yourself, your ideas and focusing on the essentials.",
+        name:"Richard Branson, CEO of Virgin Group"
+    },
+    {
+        quote:"There are a lot of things that go into creating success. I don’t like to do just the things I like to do. I like to do things that cause the company to succeed. I don’t spend a lot of time doing my favorite activities.",
+        name:"Michael Dell, CEO of Dell Computers"
+    },
+    {
+        quote:"I always did something I was a little not ready to do. I think that’s how you grow. When there’s that moment of ‘Wow, I’m not really sure I can do this,’ and you push through those moments, that’s when you have a breakthrough.",
+        name:"Marissa Mayer, CEO of Yahoo"
+    },
+    {
+        quote:"I’d highly prefer to settle versus battle … I’ve always hated litigation. We need people to invent their own stuff.",
+        name:"Tim Cook, CEO of Apple"
+    },
+    {
+        quote:"When you innovate, you’ve got to be prepared for everyone telling you you’re nuts.",
+        name:"Larry Ellison, CEO of Oracle"
+    },
+    {
+        quote:"Don't compare youself with anyone in this world...if you do so, you are insulting yourself.",
+        name:"Bill Gates, CEO of Microsoft"
+    },
+    {
+        quote:"I do not think that there is any other quality so essential to success of any kind as the quality of perseverance. It overcomes almost everything, even nature.",
+        name:"John D. Rockefeller, CEO of Standard Oil"
+    },
 ];
